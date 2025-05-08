516f333321474a414b9f7d43ea478fbae9497632
jessec01 <jegro17@gmail.com>
2025-05-07 21:04:08 -0400

    version: 0.1 docs:se reinicio todo para solucionar bug en la aplicacion

---
+++ To make polygon/build/CMakeFiles/tutorial.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jessec/Documentos/Computer-Graphics/To make polygon/render.cpp" "CMakeFiles/tutorial.dir/render.cpp.o" "gcc" "CMakeFiles/tutorial.dir/render.cpp.o.d"
+  "" "tutorial" "gcc" "CMakeFiles/tutorial.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ first demo/build/CMakeFiles/tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tutorial.dir/link.d"
+  "CMakeFiles/tutorial.dir/main.cpp.o"
+  "CMakeFiles/tutorial.dir/main.cpp.o.d"
+  "tutorial"
+  "tutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
